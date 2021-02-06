bbf4faf0be0a8b8b024fdacbdc2fdc19cb8b46ba
JLeeNZ-student <60981407+JLeeNZ-student@users.noreply.github.com>
2021-02-06 23:33:30 +1300

    Added Button B to say Duck

--- main.ts
+++ main.ts
@@ -2,4 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     basic.showString("Quack")
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Duck!")
+})
 basic.showIcon(IconNames.Duck)
